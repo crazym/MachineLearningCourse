e920d53090aedc8fc94b3acc142881b4bb2484e2
Maoting Zhang <maoting.zhang@mail.utoronto.ca>
2015-09-25 00:24:28 -0400

    final at lab

--- a1/fit.m
+++ a1/fit.m
@@ -1,19 +1,36 @@
 load('a1TrainingData.mat');
+load('a1TestData.mat');
+
 figure(1);
+% random color map (12-by-3 vector)
 cmap = hsv(12);
 for K = 1:12
     w = polynomialRegression(K, x, y)
     E = norm(y - evalPolynomial(x, w))
-    errorArray(K) = E
+    errorArray(K) = E;
+    testE = norm(yTest - evalPolynomial(xTest, w))
+    newErrorArray(K) = testE;
+    
     my_x = -2.1:0.1:2.1;
+    % plot data set regarding each degree K in a randome color in cmap
     plot(my_x, evalPolynomial(my_x, w), 'Color', cmap(K, :));
-%   make a cell array with legend info so we can call legend after the loop
+    % make a cell array with legend info so we can call legend after the loop
     legendInfo{K} = ['K = ' num2str(K)];
     hold on;
 end
 legend(legendInfo)
 title('Fitted model for x = [-2.1:0.1:2.1] in different K')
 
-figure(2)
-plot(1:12, errorArray(K))
-title('Total Residual error as a function of K')
+% error plot on training data
+figure(2);
+plot(1:12, errorArray, '--bs');
+xlabel('degree K');
+ylabel('Total Error E = norm(y-evalPolynomial(x, w)');
+title('Total Residual error on Training Data as a function of K');
+
+% total error plot test data
+figure(3);
+plot(1:12, newErrorArray, '--bs');
+xlabel('degree K');
+ylabel('Total Error E = norm(y-evalPolynomial(x, w)');
+title('Total Residual error on TestData as a function of K');
