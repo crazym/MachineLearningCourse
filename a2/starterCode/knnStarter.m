9a543f5656b2def7aeccc2f4a92a50c7a9ae1f4d
Maoting Zhang <maoting.zhang@mail.utoronto.ca>
2015-10-22 23:59:38 -0400

    knn error plot

--- a2/starterCode/knnStarter.m
+++ a2/starterCode/knnStarter.m
@@ -1,31 +1,114 @@
 
 % Use the function knnClassify to test performance on different datasets.
+figure(1);clf;
 
 
 load('a2/dataSets/generic1');
+n1t = size(c1_train, 2);
+n2t = size(c2_train, 2);
+trainingInputs = [c1_train, c2_train];
+trainingTargets = [ones(1, n1t) zeros(1, n2t); zeros(1, n1t) ones(1, n2t)];
 
-k = 2;
-
-x1 = c1_train';
-x2 = c2_train';
-
+n1 = size(c1_test, 2);
+n2 = size(c2_test, 2);
+testInputs = [c1_test, c2_test];
+testTargets = [ones(1, n1) zeros(1, n2); zeros(1, n1) ones(1, n2)];
 
 E = [];
 for k = 3:2:21
-    
-    n1 = size(c1_test, 2);
-    n2 = size(c2_test, 2);
-    
+
     num_error = 0;
-    trainingInputs = [c1_test, c2_test];
-    trainingTargets = [ones(1, n1) zeros(1, n2); zeros(1, n1) ones(1, n2)];
-    
+
     for i = 1:(n1+n2)
     %     c1_test(:, i)
-        class = knnClassify(trainingInputs(:, i)', k, trainingInputs', trainingTargets');
-        if class ~= trainingTargets(:, i)'
+        class = knnClassify(testInputs(:, i)', k, trainingInputs', trainingTargets');
+        if class ~= testTargets(:, i)'
             num_error = num_error + 1;
         end
     end
     E = [E; k num_error/(n1+n2)];
 end
+
+plot(E(:, 1), E(:, 2), '--ms');
+hold on;
+% ==============================================================
+
+load('a2/dataSets/generic2');
+
+n1t = size(c1_train, 2);
+n2t = size(c2_train, 2);
+trainingInputs = [c1_train, c2_train];
+trainingTargets = [ones(1, n1t) zeros(1, n2t); zeros(1, n1t) ones(1, n2t)];
+
+n1 = size(c1_test, 2);
+n2 = size(c2_test, 2);
+testInputs = [c1_test, c2_test];
+testTargets = [ones(1, n1) zeros(1, n2); zeros(1, n1) ones(1, n2)];
+
+E = [];
+for k = 3:2:21
+
+    num_error = 0;
+
+    for i = 1:(n1+n2)
+    %     c1_test(:, i)
+        class = knnClassify(testInputs(:, i)', k, trainingInputs', trainingTargets');
+        if class ~= testTargets(:, i)'
+            num_error = num_error + 1;
+        end
+    end
+    E = [E; k num_error/(n1+n2)];
+end
+
+plot(E(:, 1), E(:, 2), '--bs');
+hold on;
+
+% ==============================================================
+
+load('a2/dataSets/fruit_train');
+load('a2/dataSets/fruit_test');
+
+E = [];
+for k = 3:2:21
+    
+    num_error = 0;
+
+    for i = 1:size(inputs_test, 2)
+    %     c1_test(:, i)
+        class = knnClassify(inputs_test(:, i)', k, inputs_train', target_train');
+        if class ~= target_test(:, i)'
+            num_error = num_error + 1;
+        end
+    end
+    E = [E; k num_error/size(inputs_test, 2)];
+end
+
+plot(E(:, 1), E(:, 2), '--go');
+hold on;
+
+% ==============================================================
+
+load('a2/dataSets/mnist_train');
+load('a2/dataSets/mnist_test');
+
+E = [];
+for k = 3:2:21
+    
+    num_error = 0;
+
+    for i = 1:size(inputs_test, 2)
+    %     c1_test(:, i)
+        class = knnClassify(inputs_test(:, i)', k, inputs_train', target_train');
+        if class ~= target_test(:, i)'
+            num_error = num_error + 1;
+        end
+    end
+    E = [E; k num_error/size(inputs_test, 2)];
+end
+
+plot(E(:, 1), E(:, 2), '--yo');
+
+legend('generic1', 'generic2', 'fruits', 'digits');
+xlabel('k');
+ylabel('test error');
+title('Test Error of KNN as a function of k');
