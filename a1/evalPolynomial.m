fd17aeafc49ad5db2fb1abe2b8bb097d0a9027cf
Minty Zhang <minty.mz@gmail.com>
2015-11-04 21:32:17 -0500

    one-line comment to a1

--- a1/evalPolynomial.m
+++ a1/evalPolynomial.m
@@ -3,5 +3,6 @@ function y = evalPolynomial(x,w)
 % given values of x.  This function should work even if x is a vector.
 y = 0;
 for i = 1:size(w)
+    % follow the definition f(x) = \sum_{i=0}^{K} w(i) x^i
     y = y + w(i)*(x.^(i-1));
 end
